6deec58f8699d9fc5e3229b822e3cdb56e7ddf1c
yasuabe <yasu@yasuabe.net>
2017-05-12 23:46:38 +0900

    change Int to Node for Border

--- Simpath/Border.hs
+++ Simpath/Border.hs
@@ -6,13 +6,13 @@ import Simpath.Edge
 import Data.Set (Set)
 import qualified Data.Set as Set
 
-data Border = Border { edge :: Edge, done :: Maybe Int } deriving (Show)
+data Border = Border { edge :: Edge, done :: Maybe Node } deriving (Show)
 
 borders :: [Edge] -> [Border]
 borders es = reverse $ evalState bordersState Set.empty
   where
     bordersState = sequence $ map fromEdge $ reverse es
-    fromEdge :: Edge -> State (Set Int) Border
+    fromEdge :: Edge -> State (Set Node) Border
     fromEdge e@(Edge l _) = do used <- get
                                put $ Set.insert l used
                                return $ Border e $ justIf (Set.notMember l used) l
